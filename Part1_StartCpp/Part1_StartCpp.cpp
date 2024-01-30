7a452981136f22ef67942c9980d9627b7e6a9a99
gksk <Daum_c0m@naver.com>
2024-01-30 20:49:13 +0900

    005. 사칙연산

--- Part1_StartCpp/Part1_StartCpp.cpp
+++ Part1_StartCpp/Part1_StartCpp.cpp
@@ -6,46 +6,66 @@ int main()
 {
 #pragma region 001.Start C++
 
-    cout << "Hello C++ World!" << endl;
+    /*cout << "Hello C++ World!" << endl;
 
-    //return 0;
+    return 0;*/
 
 #pragma endregion
 
 #pragma region 002. cout, cin, endl
 
-    int number = 0;
+    /*int number = 0;
 
     cin >> number;
     cout << "입력한 숫자는 " << number << "입니다." << endl;
 
-    //return 0;
+    return 0;*/
 
 #pragma endregion
 
 #pragma region 003. Variable
 
-    int one = 1;
+    /*int one = 1;
     int two = 2;
     int sum = one + two;
 
     cout << "1 + 2 = " << sum << endl;
 
-    //return 0;
+    return 0;*/
 
 #pragma endregion
 
 #pragma region 004. Const
 
-    const int GREATE_VICTORY_SALSU = 612;
+    /*const int GREATE_VICTORY_SALSU = 612;
     const int GREATE_VICTORY_GWIJU = 1019;
 
     cout << "고구려 살수대첩 연도: " << GREATE_VICTORY_SALSU << "년" << endl;
     cout << "고려 귀주대첩 연도: " << GREATE_VICTORY_GWIJU << "년" << endl;
 
+    return 0;*/
+
+#pragma endregion
+
+#pragma region 005. Four basic operations
+
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
+
+    cout << "1 + 3 =" << one + two << endl;
+    cout << "4 - 2 =" << four - two << endl;
+    cout << "2 * 3 =" << two * three << endl;
+    cout << "4 / 2 =" << four / 2 << endl;
+
+    cout << "2 + 3 * 4 = " << two + three * four << endl;
+    cout << "1 + 4 / 2 = " << one + four / two << endl;
+
     return 0;
 
 #pragma endregion
+
 
 
 
