6a13332fb75746f29cade018abc6da1fde14d7b7
gksk <Daum_c0m@naver.com>
2024-01-30 20:24:38 +0900

    003. 변수 배우기

--- Part1_StartCpp/Part1_StartCpp.cpp
+++ Part1_StartCpp/Part1_StartCpp.cpp
@@ -19,8 +19,22 @@ int main()
     cin >> number;
     cout << "입력한 숫자는 " << number << "입니다." << endl;
 
-    return 0;
+    //return 0;
+
 #pragma endregion
+
+#pragma region 003. Variable
+
+    int one = 1;
+    int two = 2;
+    int sum = one + two;
+
+    cout << "1 + 2 = " << sum << endl;
+
+    return 0;
+
+#pragma endregion
+
 
     
 }
